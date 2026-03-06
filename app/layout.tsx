637acc771137e57f9b4ecfd31f9713014c4776b9
takumi334 <taguhe594@gmail.com>
2026-03-06 14:27:52 +0900

    move google verification to metadata other

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,18 +8,17 @@ import SiteHeader from "./components/SiteHeader";
 import GrammarHints from "./components/GrammarHints";
 import AdBreakProvider from "./components/AdBreakProvider";
 
-const GOOGLE_SITE_VERIFICATION = "YpiM3LqythtSfHft11x7j6oR5GgA_N6u9kqNt-M3nC4";
-
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  other: {
+    "google-site-verification": "YpiM3LqythtSfHft11x7j6oR5GgA_N6u9kqNt-M3nC4",
+  },
+};
 
 export default async function RootLayout({ children }) {
   const initialLocale = await getInitialLocale();
 
   return (
     <html lang={initialLocale} translate="no" className="notranslate" suppressHydrationWarning>
-      <head>
-        <meta name="google-site-verification" content={GOOGLE_SITE_VERIFICATION} />
-      </head>
       <body className="min-h-dvh bg-black text-white antialiased">
         {/* 👇 ここで包む */}
         <I18nUIProvider initialLocale={initialLocale}>
