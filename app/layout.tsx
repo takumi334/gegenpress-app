f92344dd7e7728385ad47cb4e06b728f772ca177
takumi334 <taguhe594@gmail.com>
2026-03-06 16:20:52 +0900

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   verification: {
     google: "YpiM3LqythtSfHft11x7j6oR5GgA_N6u9kqNt-M3nC4",
   },
+  other: {
+    "google-site-verification": "YpiM3LqythtSfHft11x7j6oR5GgA_N6u9kqNt-M3nC4",
+  },
 };
 
 export default async function RootLayout({ children }) {
@@ -27,7 +30,6 @@ export default async function RootLayout({ children }) {
       </head>
 
       <body className="min-h-dvh bg-black text-white antialiased">
-        {/* 👇 ここで包む */}
         <I18nUIProvider initialLocale={initialLocale}>
           <Suspense fallback={null}>
           <AdBreakProvider>
@@ -49,6 +51,4 @@ export default async function RootLayout({ children }) {
     </html>
   );
 }
-
-
 
